633a8b85ea6f91a5db571bcc1123186b5c43c7e4
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2024-03-08 20:08:29 -0700

    Add project files.

---
+++ InvalidRadiusException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionalHandling
+{
+    public class InvalidRadiusException
+    {
+        /*Method that returns that radius is not valid without any arugment*/
+        public string invalidradiusexpeection()
+        {
+            string a;
+            a = "The radius is less than or equal to zero ";
+            return a;
+        }
+        /*Method that returns that radius is not valid without any arugment*/
+        public string SecondConstructor( double radius)
+        {
+            string message;
+            message = $"{radius} is not valid";
+            return message;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ExceptionalHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create radiuspotive to access set radius*/
+            Circle Raiduspostive = new Circle();
+            try
+            {
+                /*this is the first run wiht taking the radius postive*/
+                Raiduspostive.SetRadius(3);
+                Console.WriteLine(Raiduspostive.ToString());
+            }
+            catch (Exception ex)
+            { 
+                /*using the ex to dispakt the error message */
+                Console.WriteLine("Error" + ex.Message);
+            }
+            /*Create radiusnegative to access set radius*/
+            Circle Raidusnegative = new Circle();
+            try
+            {
+                /*this is the second run and displating  error message by using my constrcutor that takes no arugument, 
+                *here the value of the radius is shown by using two string method*/
+                Raidusnegative.SetRadius(-4);
+                Console.WriteLine(Raidusnegative.ToString());
+            }
+            catch (Exception ex)
+            {
+                /*Created an instance mymessage from My InvlaidRadiusException class*/
+                InvalidRadiusException mymessgae = new InvalidRadiusException();
+                Console.WriteLine(mymessgae.invalidradiusexpeection());
+            }
+            /*Create zero to access set radius*/
+            Circle Raiduszero = new Circle();
+            try
+            {
+                /*here is the third run and dsplaying the error message using the cosntrcutor that takes arugment 
+                 here the arugemnt to the constructor  is given from our circle class by using the radius value*/
+                Raiduszero.SetRadius(0);
+                Console.WriteLine(Raiduszero.ToString());
+            }
+            catch (Exception ex)
+            {
+                /*Created an instance mymessage from My InvlaidRadiusException class*/
+                InvalidRadiusException mymessgae = new InvalidRadiusException();
+                Console.WriteLine(mymessgae.SecondConstructor(Raiduszero.Radius));
+            }
+
+        }
+    }
+    class Circle
+    {
+        public double Radius
+        { 
+            get;  set;
+        }
+        /*Creating an method that returns the value of radius if it grater than zero*/
+        public double SetRadius(double radius)
+        {
+            if (radius > 0)
+            {
+                Radius = radius;
+                return Radius;
+            }
+            else
+            {
+                throw new Exception("Invalid Radius Exception");
+            }
+        }
+        /*To string method to display inforamtion*/
+        public override string ToString()
+        {
+            return $"The value of the radius : {Radius}";
+        }
+    }
+}
